122765892030017c4b0aa9b9c4d2da6ad1f90eeb
Izabela K <aizeilla12344@gmail.com>
2024-04-08 02:09:55 +0200

    Added explanation to some material parameters

--- rayTracer/include/material.hpp
+++ rayTracer/include/material.hpp
@@ -10,10 +10,15 @@ public:
 
 	Material(
 		const Vector3& color = Vector3(1, 0, 0),
+		// Colour of Material under white ambient light.Usually, but not always, the same as diffuseColour.
 		const Vector3& ambientColor = Vector3(1),
+		// Colour of Material under direct white light. Usually, but not always, the same as ambientColour.
 		const Vector3& diffuseColor = Vector3(1),
+		// Colour of reflected rays under direct white light. If this is zero then there are no reflections.
 		const Vector3& reflectColor = Vector3(1),
+		// Colour of Material's specular highlights. If this is zero then there are no highlights.
 		const Vector3& specularColor = Vector3(1),
+		// 'Hardness' of Material's specular hightlights or 'Shinnines' of the material - high values give small, sharp highlights.
 		const float specularExponent = 2.f,
 		const float mirrorReflection = 1.f,
 		const float diffuseReflection = 1.f
